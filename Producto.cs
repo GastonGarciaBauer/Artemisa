86256d7ad7cf1c5444ce87f66e2b26ba0d4eaf9c
Gastón García Bauer <gaston.garcia89@hotmail.com>
2025-07-10 19:54:25 -0300

    Realiza lógica del main y crea clases Producto, Trago y Ticket.Agregar archivos de proyecto.

---
+++ Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artemisa
+{
+    abstract class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int valor { get; set; }
+        public Producto(int id, string nombre, int valor)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.valor = valor;
+
+        }
+        public abstract double CalcularPrecioFinal();
+
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+
+namespace Artemisa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket ticket = new Ticket();
+
+            Console.WriteLine("\n\t\t\t |Artemisa - Tragos de Autor|\n");
+            Console.WriteLine("\nIngresá tu nombre: \n");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("\nIngresá el día de la semana actual: \n");
+            string dia = Console.ReadLine();
+
+
+            Console.Clear();
+
+            bool agregarMas = true;
+
+            Console.WriteLine("\n\t\t\t |Artemisa - Tragos de Autor|\n");
+
+            Console.WriteLine("\n\t\t\t |TRAGOS|\n");
+            Console.WriteLine($"\nHola {nombre}, Bienvenide!!!. Por favor, realizá tu pedido por número de  trago: \n");
+
+            do
+            {
+                Console.WriteLine("\n1. Cascada Oriental\n");
+                Console.WriteLine("\n2. Exilio SurCoreano\n");
+                Console.WriteLine("\n3. Aguasturbias\n");
+                Console.WriteLine("\n4. Colorado on the Rocks\n");
+                Console.WriteLine("\n5. Cucumelo Indígena\n");
+                Console.WriteLine("\n6. Jesus Christ Blood\n");
+                Console.WriteLine("\n7. Amnistía Internacional\n");
+                int trago = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nPor favor, ingresá la cantidad de unidades que desees: \n");
+                int cantidad = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (trago)
+                {
+                    case 1:
+                        Producto trago1 = new Trago(1, "Cascada Oriental", 3200, dia);
+
+                        ticket.AgregarTrago(trago1);
+
+                        Console.WriteLine($"\nValor del trago: {trago1.valor}\n");
+
+                        Console.WriteLine($"\nValor con descuento: {((Trago)trago1).CalcularPrecioFinal()}\n");
+
+                        Console.WriteLine($"Subtotal: {(((Trago)trago1).CalcularPrecioFinal()) * cantidad}");
+
+
+                        break;
+                    case 2:
+                        Producto trago2 = new Trago(2, "Exilio SurCoreano", 3100, dia);
+
+                        ticket.AgregarTrago(trago2);
+
+                        Console.WriteLine($"\nValor del trago: {trago2.valor}\n");
+
+                        Console.WriteLine($"\nValor con descuento: {((Trago)trago2).CalcularPrecioFinal()}\n");
+
+                        Console.WriteLine($"Subtotal: {(((Trago)trago2).CalcularPrecioFinal()) * cantidad}");
+                        break;
+                    case 3:
+                        Producto trago3 = new Trago(3, "Aguasturbias", 3000, dia);
+
+                        ticket.AgregarTrago(trago3);
+
+                        Console.WriteLine($"\nValor del trago: {trago3.valor}\n");
+
+                        Console.WriteLine($"\nValor con descuento: {((Trago)trago3).CalcularPrecioFinal()}\n");
+
+                        Console.WriteLine($"Subtotal: {(((Trago)trago3).CalcularPrecioFinal()) * cantidad}");
+                        break;
+                    case 4:
+                        Producto trago4 = new Trago(4, "Colorado on the Rocks", 3400, dia);
+
+                        ticket.AgregarTrago(trago4);
+
+                        Console.WriteLine($"\nValor del trago: {trago4.valor}\n");
+
+                        Console.WriteLine($"\nValor con descuento: {((Trago)trago4).CalcularPrecioFinal()}\n");
+
+                        Console.WriteLine($"Subtotal: {(((Trago)trago4).CalcularPrecioFinal()) * cantidad}");
+                        break;
+                    case 5:
+                        Producto trago5 = new Trago(5, "Cucumelo Indígena", 3500, dia);
+
+                        ticket.AgregarTrago(trago5);
+
+                        Console.WriteLine($"\nValor del trago: {trago5.valor}\n");
+
+                        Console.WriteLine($"\nValor con descuento: {((Trago)trago5).CalcularPrecioFinal()}\n");
+
+                        Console.WriteLine($"Subtotal: {(((Trago)trago5).CalcularPrecioFinal()) * cantidad}");
+                        break;
+                    case 6:
+                        Producto trago6 = new Trago(6, "Jesus Christ Blood", 3700, dia);
+
+                        ticket.AgregarTrago(trago6);
+
+                        Console.WriteLine($"\nValor del trago: {trago6.valor}\n");
+
+                        Console.WriteLine($"\nValor con descuento: {((Trago)trago6).CalcularPrecioFinal()}\n");
+
+                        Console.WriteLine($"Subtotal: {(((Trago)trago6).CalcularPrecioFinal()) * cantidad}");
+                        break;
+                    case 7:
+                        Producto trago7 = new Trago(7, "Amnistía Internacional", 3800, dia);
+
+                        ticket.AgregarTrago(trago7);
+
+                        Console.WriteLine($"\nValor del trago: {trago7.valor}\n");
+
+                        Console.WriteLine($"\nValor con descuento: {((Trago)trago7).CalcularPrecioFinal()}\n");
+
+                        Console.WriteLine($"Subtotal: {(((Trago)trago7).CalcularPrecioFinal()) * cantidad}");
+                        break;
+                    default:
+                        Console.WriteLine("\nPor favor, ingresá un número de Producto válido: \n");
+                        break;
+                }
+                Console.WriteLine("\nDeseas agregar más tragos? s/n\n");
+                string eleccion = Console.ReadLine();
+                Console.Clear();
+
+                if (eleccion.ToLower() == "s")
+                {
+                    agregarMas = true;
+                }
+                else
+                {
+                    agregarMas = false;
+                }
+            }
+
+            while (agregarMas == true);
+
+            Console.Clear();
+
+            ticket.MostrarTicket();
+        }
+    }
+}
+
+// mostrar una carta de tragos
+//el usuario puede escribir el numero del trago o el nombre del trago
+//el usuario ingresa el día, los tragos y la cantidad de cada uno.
+//se le calcula el precio total con descuento por promoción (combinación de trago) y promoción (día de la semana)
+//se muestran los nombres de los tragos, con la cantidad de cada uno, el subtotal por trago y el total final.
